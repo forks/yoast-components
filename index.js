259d71bbc61d95dd3b1264c82a6c8e2bdc46fa1a
Rens Weerman <rens@yoast.com>
2016-08-23 15:34:30 +0200

    export config

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 import OnboardingWizard from "./composites/OnboardingWizard/OnboardingWizard";
+import config from "./composites/OnboardingWizard/config/production-config";
 
-export {OnboardingWizard};
+export {OnboardingWizard, config};
